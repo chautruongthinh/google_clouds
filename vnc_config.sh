ae8aa87db21858b39f88af999f13e79b1c0a984c
Chau Truong Thinh <chautruongthinh@gmail.com>
2016-10-20 22:06:01 +0700

    Add Chrome web browser

--- vnc_config.sh
+++ vnc_config.sh
@@ -16,3 +16,9 @@ sudo tar xzvf elementary-xfce.tar.gz -C /usr/share/icons/
 rm -rf bluebird.tar.gz
 rm -rf elementary-xfce.tar.gz
 
+#Chrome web browser
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update && sudo apt-get install google-chrome-stable
+
+
