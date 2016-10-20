afea75d9913a7fd4e81cb8cc8e3739982168126b
Chau Truong Thinh <chautruongthinh@gmail.com>
2016-10-20 22:51:37 +0700

    Add Gdrive && delete not use deb

--- setup_build_for_14.0.4.sh
+++ setup_build_for_14.0.4.sh
@@ -8,6 +8,9 @@ sudo dpkg -i ~/openjdk-8-jre_8u45-b14-1_amd64.deb
 sudo dpkg -i ~/openjdk-8-jdk_8u45-b14-1_amd64.deb
 sudo apt-get -f install
 sudo apt-get install phablet-tools git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip schedtool meld lzop maven2 -y
+sudo rm -rf ~/openjdk-8-jre-headless_8u45-b14-1_amd64.deb
+sudo rm -rf ~/openjdk-8-jre_8u45-b14-1_amd64.deb
+sudo rm -rf ~/openjdk-8-jdk_8u45-b14-1_amd64.deb
 mkdir cm-14.0 && cd cm-14.0
 git config --global user.name "Chau Truong Thinh"
 git config --global user.email "chautruongthinh@gmail.com"
@@ -16,6 +19,7 @@ cd .repo
 git clone https://github.com/chautruongthinh/local_manifests.git -b cm-14.0
 cd ..
 repo sync -j32
+cd ~
 
 # use ccache
 export USE_CCACHE=1
@@ -24,3 +28,8 @@ export CCACHE_DIR=~/.ccache
 rm -rf ~/.bashrc
 wget https://raw.githubusercontent.com/chautruongthinh/google_clouds/master/.bashrc
 
+# Google Drive
+wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
+mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive
+chmod +x gdrive
+sudo install gdrive /usr/local/bin/gdrive
