ff23822e2930fe4ea5d8c48c311a2e39d3807a59
chautruongthinh <chautruongthinh@gmail.com>
2017-03-06 14:48:56 +0700

    Update for LineageOS

--- setup_build_for_14.0.4.sh
+++ setup_build_for_14.0.4.sh
@@ -6,15 +6,15 @@ sudo apt-get update
 sudo dpkg -i ~/openjdk-8-jre-headless_8u45-b14-1_amd64.deb
 sudo dpkg -i ~/openjdk-8-jre_8u45-b14-1_amd64.deb
 sudo dpkg -i ~/openjdk-8-jdk_8u45-b14-1_amd64.deb
-sudo apt-get -f install
-sudo apt-get install phablet-tools git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip schedtool meld lzop maven2 -y
+sudo apt-get -f install -y
+sudo apt-get -f install phablet-tools git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip schedtool meld lzop maven2 -y
 sudo rm -rf ~/openjdk-8-jre-headless_8u45-b14-1_amd64.deb
 sudo rm -rf ~/openjdk-8-jre_8u45-b14-1_amd64.deb
 sudo rm -rf ~/openjdk-8-jdk_8u45-b14-1_amd64.deb
 mkdir cm-14.1 && cd cm-14.1
 git config --global user.name "Chau Truong Thinh"
 git config --global user.email "chautruongthinh@gmail.com"
-repo init -u git://github.com/CyanogenMod/android.git -b cm-14.1
+repo init -u git://github.com/LineageOS/android.git -b cm-14.1
 git clone https://github.com/chautruongthinh/local_manifests.git -b cm-14.1 .repo/local_manifests
 repo sync -j32
 cd ~
@@ -30,3 +30,5 @@ wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
 mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive
 chmod +x gdrive
 sudo install gdrive /usr/local/bin/gdrive
+rm -rf gdrive
+
