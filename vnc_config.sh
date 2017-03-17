a183186f7de9f4b856e548cb2720f695ad473aa0
Chau Truong Thinh <chautruongthinh@gmail.com>
2017-03-17 12:29:23 +0700

    Set correct branch for download file

--- vnc_config.sh
+++ vnc_config.sh
@@ -4,13 +4,13 @@ sudo apt-get install xfce4 xfce4-goodies tightvncserver -y
 vncserver
 vncserver -kill :1
 mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
-wget -O ~/.vnc/xstartup https://raw.githubusercontent.com/chautruongthinh/google_clouds/master/xstartup
-sudo wget -O /etc/init.d/vncserver https://raw.githubusercontent.com/chautruongthinh/google_clouds/master/vncserver
+wget -O ~/.vnc/xstartup https://raw.githubusercontent.com/chautruongthinh/google_clouds/ubuntu_14.0.4/xstartup
+sudo wget -O /etc/init.d/vncserver https://raw.githubusercontent.com/chautruongthinh/google_clouds/ubuntu_14.0.4/vncserver
 sudo chmod +x ~/.vnc/xstartup
 sudo chmod +x /etc/init.d/vncserver
 sudo service vncserver start
-wget https://raw.githubusercontent.com/chautruongthinh/google_clouds/master/bluebird.tar.gz
-wget https://raw.githubusercontent.com/chautruongthinh/google_clouds/master/elementary-xfce.tar.gz
+wget https://raw.githubusercontent.com/chautruongthinh/google_clouds/ubuntu_14.0.4/bluebird.tar.gz
+wget https://raw.githubusercontent.com/chautruongthinh/google_clouds/ubuntu_14.0.4/elementary-xfce.tar.gz
 sudo tar xzvf bluebird.tar.gz -C /usr/share/themes/
 sudo tar xzvf elementary-xfce.tar.gz -C /usr/share/icons/
 rm -rf bluebird.tar.gz
