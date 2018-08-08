382e6254b14f7dada7c55492b976178e5ea07772
chautruongthinh <chautruongthinh@gmail.com>
2018-08-08 22:17:18 +0700

    Fix typo

--- setup_build_for_16.0.4.sh
+++ setup_build_for_16.0.4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk phablet-tools git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip schedtool meld lzop maven -y
-mkdir $1 && cd $1 
+mkdir lineage-15.1 && cd lineage-15.1 
 git config --global user.name "Chau Truong Thinh"
 git config --global user.email "chautruongthinh@gmail.com"
 repo init -u git://github.com/LineageOS/android.git -b lineage-15.1
